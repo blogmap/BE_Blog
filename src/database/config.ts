42ca984753fcc3b5c01a13d7e5a3919293233d69
Jaehee Lee <55607202+ij5@users.noreply.github.com>
2024-11-20 14:28:59 +0900

    migrate hard coded secrets to .env

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,15 +1,15 @@
-import { DataSource } from "typeorm";
+import { DataSource } from 'typeorm';
 
 export const AppDataSource = new DataSource({
-    type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "postgres",
-    password: "hothanhtienqb123",
-    database: "postgres",
-    synchronize: true,  
-    logging: true,
-    entities: [__dirname + "/models/*.{js,ts}"],  
-    subscribers: [],
-    migrations: [],
+  type: 'postgres',
+  host: process.env.DB_HOST,
+  port: 5432,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  synchronize: true,
+  logging: true,
+  entities: [__dirname + '/models/*.{js,ts}'],
+  subscribers: [],
+  migrations: [],
 });
--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,29 @@
-import express from "express";
+import express from 'express';
 import path from 'path';
 import fs from 'fs';
 import cors from 'cors';
-import bodyParser from "body-parser";
-import cookieParser from "cookie-parser";
-import { AppDataSource } from "./database/config";
-import { authenticateJWT } from "./middlewares/authenticateJWT";
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import { AppDataSource } from './database/config';
+import { authenticateJWT } from './middlewares/authenticateJWT';
+import dotenv from 'dotenv';
 // import { isAuth } from "./middlewares/isAuth";
 // import auth from "./routes/public/auth";
+
+dotenv.config();
 
 const app = express();
 
 app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
-app.get("/hello", (req, res) => {
-  res.json({ message: "world" });
-}); 
+app.get('/hello', (req, res) => {
+  res.json({ message: 'world' });
+});
 
 AppDataSource.initialize()
-.then(() => {
-    const RouterPath = path.resolve(__dirname, "modules");
+  .then(() => {
+    const RouterPath = path.resolve(__dirname, 'modules');
     const Routers: Array<string> = fs.readdirSync(RouterPath);
 
     for (const router of Routers) {
@@ -28,10 +31,9 @@ AppDataSource.initialize()
       app.use(`/${router}`, req_router);
     }
 
-
-    console.log('ok')
+    console.log('ok');
     app.listen(4000, () => {
-      console.log("Server is running on port 4000");
+      console.log('Server is running on port 4000');
     });
-})
-.catch((error) => console.log("Error initializing data source:", error));
+  })
+  .catch((error) => console.log('Error initializing data source:', error));
