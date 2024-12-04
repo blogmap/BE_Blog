47fb9b4b1bef5d2328b3583377c740128d636781
Ho Thanh Tien <hothanhtienqb@gmail.com>
2024-12-04 15:42:22 +0700

    fix typeOrm

--- src/database/config.ts
+++ src/database/config.ts
@@ -1,5 +1,7 @@
 import { DataSource } from 'typeorm';
+import dotenv from 'dotenv';
 
+dotenv.config();
 export const AppDataSource = new DataSource({
   type: 'postgres',
   host: process.env.DB_HOST,
