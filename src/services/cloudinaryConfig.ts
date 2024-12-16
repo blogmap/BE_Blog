9a866b3757cf76edad0b5b9c2c6f6308ddeb5a28
Ho Thanh Tien <hothanhtienqb@gmail.com>
2024-12-16 09:48:35 +0700

    fixx env couldinary

--- src/services/cloudinaryConfig.ts
+++ src/services/cloudinaryConfig.ts
@@ -4,9 +4,9 @@ import dotenv from "dotenv";
 dotenv.config();
 
 cloudinary.config({
-  cloud_name: process.env.CLOUDINARY_CLOUD_NAME || "your_cloud_name",
-  api_key: process.env.CLOUDINARY_API_KEY || "your_api_key",
-  api_secret: process.env.CLOUDINARY_API_SECRET || "your_api_secret",
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
 export default cloudinary;
